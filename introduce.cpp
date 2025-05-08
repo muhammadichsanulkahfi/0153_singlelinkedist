372ae9b542e71dbb6fa07a02008d42faeec1aa26
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-05-08 15:13:37 +0700

    COMIT 1

---
+++ introduce.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *nexet;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    List()
+    {
+        START = NULL;
+    }
+
+    
+
+}
